3f5e079bb59bf643e72862f739c338fbd78839b0
ThomasCochou <cochouthomas@gmail.com>
2021-10-26 21:22:53 +0200

    lisibility

--- art_gan.py
+++ art_gan.py
@@ -4,8 +4,6 @@
 from tensorflow import keras
 
 from tensorflow.keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormalization, Activation, ZeroPadding2D, Conv2D, UpSampling2D, LeakyReLU
-# from tensorflow.keras.layers.advanced_activations import LeakyReLU
-# from tensorflow.keras.layers.convolutional import , 
 from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.optimizers import Adam
 import numpy as np
@@ -21,66 +19,85 @@ SAVE_FREQ = 100
 NOISE_SIZE = 100
 # Configuration
 EPOCHS = 10000 # number of iterations
-BATCH_SIZE = 1 #32 at first
+BATCH_SIZE = 2 #32 at first
 GENERATE_RES = 3
 IMAGE_SIZE = 128 # rows/cols
 IMAGE_CHANNELS = 3
 
 training_data = np.load('Agnes_Lawrence_Pelton_pictures.npy')
 
+
+
+
 def build_discriminator(image_shape):
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=3, strides=2,
-    input_shape=image_shape, padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(64, kernel_size=3, strides=2, padding='same'))
-    model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(128, kernel_size=3, strides=2, padding='same'))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(256, kernel_size=3, strides=1, padding='same'))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(512, kernel_size=3, strides=1, padding='same'))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
-    input_image = Input(shape=image_shape)
-    validity = model(input_image)
-    return Model(input_image, validity)
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=3, strides=2,
+input_shape=image_shape, padding='same'))
+model.add(LeakyReLU(alpha=0.2))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, kernel_size=3, strides=2, padding='same'))
+model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
+model.add(BatchNormalization(momentum=0.8))
+model.add(LeakyReLU(alpha=0.2))
+model.add(Dropout(0.25))
+model.add(Conv2D(128, kernel_size=3, strides=2, padding='same'))
+model.add(BatchNormalization(momentum=0.8))
+model.add(LeakyReLU(alpha=0.2))
+model.add(Dropout(0.25))
+model.add(Conv2D(256, kernel_size=3, strides=1, padding='same'))
+model.add(BatchNormalization(momentum=0.8))
+model.add(LeakyReLU(alpha=0.2))
+model.add(Dropout(0.25))
+model.add(Conv2D(512, kernel_size=3, strides=1, padding='same'))
+model.add(BatchNormalization(momentum=0.8))
+model.add(LeakyReLU(alpha=0.2))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(1, activation='sigmoid'))
+
+model.summary()
+
+input_image = Input(shape=image_shape)
+validity = model(input_image)
+
+return Model(input_image, validity)
+
+
+
 
 def build_generator(noise_size, channels):
-    model = Sequential()
-    model.add(Dense(4 * 4 * 256, activation='relu',       input_dim=noise_size))
-    model.add(Reshape((4, 4, 256)))
-    model.add(UpSampling2D())
-    model.add(Conv2D(256, kernel_size=3, padding='same'))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation('relu'))
-    model.add(UpSampling2D())
-    model.add(Conv2D(256, kernel_size=3, padding='same'))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation('relu'))
-    for i in range(GENERATE_RES):
-         model.add(UpSampling2D())
-         model.add(Conv2D(256, kernel_size=3, padding='same'))
-         model.add(BatchNormalization(momentum=0.8))
-         model.add(Activation('relu'))
-    model.summary()
-    model.add(Conv2D(channels, kernel_size=3, padding='same'))
-    model.add(Activation('tanh'))
-    input = Input(shape=(noise_size,))
-    generated_image = model(input)
-    
-    return Model(input, generated_image)
+model = Sequential()
+
+model.add(Dense(4 * 4 * 256, activation='relu',input_dim=noise_size))
+model.add(Reshape((4, 4, 256)))
+model.add(UpSampling2D())
+model.add(Conv2D(256, kernel_size=3, padding='same'))
+model.add(BatchNormalization(momentum=0.8))
+model.add(Activation('relu'))
+model.add(UpSampling2D())
+model.add(Conv2D(256, kernel_size=3, padding='same'))
+model.add(BatchNormalization(momentum=0.8))
+model.add(Activation('relu'))
+
+for i in range(GENERATE_RES):
+	model.add(UpSampling2D())
+	model.add(Conv2D(256, kernel_size=3, padding='same'))
+	model.add(BatchNormalization(momentum=0.8))
+	model.add(Activation('relu'))
+
+model.add(Conv2D(channels, kernel_size=3, padding='same'))
+model.add(Activation('tanh'))
+
+model.summary()
+
+input = Input(shape=(noise_size,))
+generated_image = model(input)
+
+return Model(input, generated_image)
+
+
+
 
 def save_images(cnt, noise):
 	image_array = np.full((
@@ -106,23 +123,31 @@ def save_images(cnt, noise):
 				im = Image.fromarray(image_array)
 				im.save(filename)
 
+
+
+
 image_shape = (IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
 optimizer = Adam(1.5e-4, 0.5)
+
 discriminator = build_discriminator(image_shape)
-discriminator.compile(loss='binary_crossentropy',
-optimizer=optimizer, metrics=['accuracy'])
+discriminator.compile(loss='binary_crossentropy',optimizer=optimizer, metrics=['accuracy'])
+
 generator = build_generator(NOISE_SIZE, IMAGE_CHANNELS)
 random_input = Input(shape=(NOISE_SIZE,))
 generated_image = generator(random_input)
+
 discriminator.trainable = False
+
 validity = discriminator(generated_image)
+
 combined = Model(random_input, validity)
-combined.compile(loss='binary_crossentropy',
-optimizer=optimizer, metrics=['accuracy'])
+combined.compile(loss='binary_crossentropy',optimizer=optimizer, metrics=['accuracy'])
+
 y_real = np.ones((BATCH_SIZE, 1))
 y_fake = np.zeros((BATCH_SIZE, 1))
 fixed_noise = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, NOISE_SIZE))
 cnt = 1
+
 for epoch in range(EPOCHS):
 	idx = np.random.randint(0, training_data.shape[0], BATCH_SIZE)
 	x_real = training_data[idx]
@@ -136,8 +161,8 @@ for epoch in range(EPOCHS):
 	discriminator_metric = 0.5 * np.add(discriminator_metric_real, discriminator_metric_generated)
 	generator_metric = combined.train_on_batch(noise, y_real)
 
-if epoch % SAVE_FREQ == 0:
-   save_images(cnt, fixed_noise)
-   cnt += 1
- 
-   print(f'{epoch} epoch, Discriminator accuracy: {100*  discriminator_metric[1]}, Generator accuracy: {100 * generator_metric[1]}')
+	if epoch % SAVE_FREQ == 0:
+		save_images(cnt, fixed_noise)
+		cnt += 1
+
+		print(f'{epoch} epoch, Discriminator accuracy: {100*  discriminator_metric[1]}, Generator accuracy: {100 * generator_metric[1]}')
