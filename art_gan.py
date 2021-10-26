5c00943086be02bfffcec146d4002ef4a17be792
ThomasCochou <cochouthomas@gmail.com>
2021-10-26 15:11:37 +0200

    fix

--- art_gan.py
+++ art_gan.py
@@ -3,9 +3,9 @@
 
 from tensorflow import keras
 
-from tensorflow.keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormalization, Activation, ZeroPadding2D
+from tensorflow.keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormalization, Activation, ZeroPadding2D, Conv2D, UpSampling2D
 # from tensorflow.keras.layers.advanced_activations import LeakyReLU
-from tensorflow.keras.layers.convolutional import UpSampling2D, Conv2D
+# from tensorflow.keras.layers.convolutional import , 
 from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.optimizers import Adam
 import numpy as np
