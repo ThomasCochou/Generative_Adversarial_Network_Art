a7e05dfcddc5d85c872dfb6efb638cbd625ed706
ThomasCochou <cochouthomas@gmail.com>
2021-10-27 23:05:13 +0200

    comments

--- art_gan.py
+++ art_gan.py
@@ -10,26 +10,28 @@ import numpy as np
 from PIL import Image
 import os
 
-# Preview image Frame
+# Preview image Frame (output images eachs number of iterations)
 PREVIEW_ROWS = 4
 PREVIEW_COLS = 7
 PREVIEW_MARGIN = 4
 SAVE_FREQ = 100
-# Size vector to generate images from
+
+# Size vector to generate images from (generator input)
 NOISE_SIZE = 100
+
 # Configuration
 EPOCHS = 10000 # number of iterations
-BATCH_SIZE = 1
-GENERATE_RES = 3
+BATCH_SIZE = 3 # -> default 32
+GENERATE_RES = 3 # add layers to the generator (generate resolution) -> default 3
 IMAGE_SIZE = 128 # rows/cols
 IMAGE_CHANNELS = 3
 
 training_data = np.load('henri_matisse_abstract_expressionism.npy')
 
-
-
-
 def build_discriminator(image_shape):
+# The discriminator is a binary classifier. It needs to classify either the data is real or fake
+# Input = image (true & false)
+# Output = probability to be true or false (sigmoid)
 	model = Sequential()
 
 	model.add(Conv2D(32, kernel_size=3, strides=2,
@@ -67,6 +69,9 @@ def build_discriminator(image_shape):
 
 
 def build_generator(noise_size, channels):
+# The generator is responsible for generating different kinds of noise data
+# Input = noise (noise_shape vector with random values)
+# Output = generated image 
 	model = Sequential()
 
 	model.add(Dense(4 * 4 * 256, activation='relu',input_dim=noise_size))
@@ -100,21 +105,24 @@ def build_generator(noise_size, channels):
 
 
 def save_images(cnt, noise):
+	# create a shape to print the output
 	image_array = np.full((
 		PREVIEW_MARGIN + (PREVIEW_ROWS * (IMAGE_SIZE + PREVIEW_MARGIN)),
 		PREVIEW_MARGIN + (PREVIEW_COLS * (IMAGE_SIZE + PREVIEW_MARGIN)), 3),
 	255, dtype=np.uint8)
+	# generate image from a random noise to see the network progress
+	generated_images = generator.predict(noise) # Generate 28 images, why 28 ? shape=(28,128,128,3)
 
-	generated_images = generator.predict(noise)
+	# normalized data
 	generated_images = 0.5 * generated_images + 0.5
 
+	# for the number of image to output print the generated images
 	image_count = 0
 	for row in range(PREVIEW_ROWS):
 		for col in range(PREVIEW_COLS):
 			r = row * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
 			c = col * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
-			image_array[r:r + IMAGE_SIZE, c:c +
-			IMAGE_SIZE] = generated_images[image_count] * 255
+			image_array[r:r + IMAGE_SIZE, c:c + IMAGE_SIZE] = generated_images[image_count] * 255
 			image_count += 1
 	
 	output_path = 'output'
@@ -127,44 +135,63 @@ def save_images(cnt, noise):
 
 
 
-
+#input shape of images
 image_shape = (IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
+
 optimizer = Adam(1.5e-4, 0.5)
 
+#build discriminator with input shape
 discriminator = build_discriminator(image_shape)
 discriminator.compile(loss='binary_crossentropy',optimizer=optimizer, metrics=['accuracy'])
 
+# build generator with input shape and link it random noise output
 generator = build_generator(NOISE_SIZE, IMAGE_CHANNELS)
 random_input = Input(shape=(NOISE_SIZE,))
 generated_image = generator(random_input)
 
+# since we are only training generators here, we do not want to adjust the weights of the discriminator
 discriminator.trainable = False
 
+# link the discriminator to generator output
 validity = discriminator(generated_image)
 
+# Combined model
+# Input = random noise 
+# Output = validity, is it a real picture ?
 combined = Model(random_input, validity)
 combined.compile(loss='binary_crossentropy',optimizer=optimizer, metrics=['accuracy'])
 
+# create batch_size n list of 1's and 0's
 y_real = np.ones((BATCH_SIZE, 1))
 y_fake = np.zeros((BATCH_SIZE, 1))
-fixed_noise = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, NOISE_SIZE))
-cnt = 1
 
+# create a constant noise to inject in the generator to get an output for the saving image function
+fixed_noise = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, NOISE_SIZE))
+
+cnt = 1
 for epoch in range(EPOCHS):
+	# get some random real data
 	idx = np.random.randint(0, training_data.shape[0], BATCH_SIZE)
 	x_real = training_data[idx]
 
+	# get some random false data
 	noise= np.random.normal(0, 1, (BATCH_SIZE, NOISE_SIZE))
 	x_fake = generator.predict(noise)
 
+	# train on batch the discriminator
+	# some research has shown that training them separately (real and fake data) can get us some better results
 	discriminator_metric_real = discriminator.train_on_batch(x_real, y_real)
 	discriminator_metric_generated = discriminator.train_on_batch(x_fake, y_fake)
 
+	# compute the means of the metrics
 	discriminator_metric = 0.5 * np.add(discriminator_metric_real, discriminator_metric_generated)
+
+	# train on batch the generator
 	generator_metric = combined.train_on_batch(noise, y_real)
 
+	# save output of the generator with random noise every SAVE_FREQ n epochs and display accuracy
 	if epoch % SAVE_FREQ == 0:
 		save_images(cnt, fixed_noise)
 		cnt += 1
-
+		print(f'{epoch} epoch, discriminator_metric[0]: {100*  discriminator_metric[0]}, generator_metric[0]: {100 * generator_metric[0]}')
 		print(f'{epoch} epoch, Discriminator accuracy: {100*  discriminator_metric[1]}, Generator accuracy: {100 * generator_metric[1]}')
